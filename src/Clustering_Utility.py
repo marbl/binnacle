7396183daed53a23d03c1bb089841d87403d37e5
Harihara Subrahmaniam <harihara@wireless-10-105-39-127.umd.edu>
2020-09-09 17:17:54 -0400

    Modified Clustering_Utility.py to compute variance

--- src/Clustering_Utility.py
+++ src/Clustering_Utility.py
@@ -53,9 +53,11 @@ def Format_Outputs(summary_dir, binning_method):
     Function to format outputs to the binning method specified. 
     Input:
         binnning_method: Choice of binning method (metabat, concoct, maxbin)
+        coords_path: Coordinate file for the scaffolds
         summary_path: The path to the txt file containing the summary information by running binnacle on the original sample.
-    Output:
-        df_summary: Collated summary file
+        align_flag: A flag that specifes whether to peform all vs all alignments
+        align_dir: Directory that contains the output of running genomecov -d on the bed files obtaining mapping reads of all samples to contigs of all samples
+        output_dir: Location where the putputs of binnacle is written to.
     '''
     files = listdir(summary_dir)
     files.sort()
@@ -65,18 +67,23 @@ def Format_Outputs(summary_dir, binning_method):
         if 'Summary.txt' in files[i] and files[i][0] != '.':
             print(files[i])
             col_prefix = files[i].replace("_Summary.txt","")
-            df = pd.read_csv(summary_dir+files[i], names = ['Scaffold','Span', col_prefix+'_Mu', col_prefix+'_Sigma'],
+            df = pd.read_csv(summary_dir+files[i], names = ['Scaffold','Span', col_prefix+'_Mu', col_prefix+'_Var'],
                              sep='\t', index_col = 'Scaffold')
+            df[col_prefix+'_Var'] = df[col_prefix+'_Var']*df[col_prefix+'_Var']
             print(df.head())
             if ctr > 0:
                 del df['Span']
             if ctr == 0:
                 df['Avg_Depth'] = 0
-                df = df[['Span','Avg_Depth',col_prefix+'_Mu',col_prefix+'_Sigma']]
+                df = df[['Span','Avg_Depth',col_prefix+'_Mu',col_prefix+'_Var']]
             ctr += 1
             df_summary = df_summary.join(df, how = 'outer')
     df_mu = df_summary.filter(regex='_Mu')
-    if binning_method.lower().startswith('metabat'): df_summary['Avg_Depth'] = df_mu.mean(axis=1)
-    elif binning_method.lower().startswith('maxbin') or binning_method.lower().startswith('concoct'): df_summary = df_mu
-    else: del df_summary['Span'], df_summary['Avg_Depth']
+    if binning_method.lower().startswith('metabat'):
+        df_summary['Avg_Depth'] = df_mu.mean(axis=1)
+    elif binning_method.lower().startswith('maxbin') or binning_method.lower().startswith('concoct'):
+        df_summary = df_mu
+    else:
+        del df_summary['Span'], df_summary['Avg_Depth']
+    print(df_summary.head())
     return df_summary
